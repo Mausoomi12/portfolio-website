d5a45d322a17f603926e985e49d614d4d29141ec
Mausoomi Roy <mausoomiroy68613@gmail.com>
2022-10-01 15:53:09 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a React JS Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a React Developer</h2>
                 <p className="text-gray-500 py-4 max-w-md">
                     I am a third year student at Indore Institute of Science and Technology.
                     Currently, I'm pursuing my Bachelor degree in Computer Science.
