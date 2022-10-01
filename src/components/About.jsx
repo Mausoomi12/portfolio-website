6dd598fb576fa2039c2bc4163e857aa4a479c9f7
Mausoomi Roy <mausoomiroy68613@gmail.com>
2022-10-01 15:12:03 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                About
+            </p>
+            </div>
+
+        <p className="text-xl mt-20">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores officiis non odio quia necessitatibus similique quae quidem voluptate adipisci, autem impedit minima eos perspiciatis in rerum aliquam. Nulla molestias possimus quisquam incidunt. Itaque repellat quibusdam laudantium id exercitationem fuga labore. Modi, beatae molestiae? Quam, cumque esse corrupti voluptate voluptatum nemo!
+        </p>
+        
+        <br />
+
+        <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum est quam reiciendis nulla cum libero enim officia magni aperiam dolore minima sit dolorem expedita, veniam fugit quia quod. Sint minus perferendis commodi accusantium optio vel, officiis rem architecto repellendus? Quam quia delectus hic vero magnam pariatur error rem dolores fugit.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
