43399811ed693b17b991415147598faf7df2b4cc
Mausoomi Roy <mausoomiroy68613@gmail.com>
2022-10-12 21:07:43 +0530

    minor refactor

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg';
-import installNode from '../assets/portfolio/installNode.jpg';
 import navbar from '../assets/portfolio/navbar.jpg';
 import reactParallax from '../assets/portfolio/reactParallax.jpg';
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg';
-import reactWeather from '../assets/portfolio/reactWeather.jpg';
+
 
 
 const Portfolio = () => {
