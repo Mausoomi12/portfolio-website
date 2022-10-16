966e6ab594a2afa5b582fb8f9a950bc608e30642
Mausoomi Roy <mausoomiroy68613@gmail.com>
2022-10-16 19:58:37 +0530

    minor refactor

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,9 @@ const Contact = () => {
   return (
     <div name='contact' className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-            <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500"><br/><br/><br/>Contact</p>
+          
+            <div className="pt-20 pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
                 <p className="py-6">Submit the form below to get in touch with me</p>
             </div>
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -81,12 +81,12 @@ const Experience = () => {
      name='experience' 
      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
 
-       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white pt-80 pb-40">
         <div>
             <p className="text-4xl font-bold border-b-4 border-gray-500 p-0 inline">
-                <br/><br/><br/><br/>Experience
+                <br/>Experience
             </p>
-            <p className="py-6">These are the technologies I've worked with</p>
+            <p className="py-4">These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
