9e7e5ebab895c053740e9be61edf87f16a54c3d4
Mausoomi Roy <mausoomiroy68613@gmail.com>
2023-06-22 00:12:44 +0530

    put project details in portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,62 +1,56 @@
 import React from 'react';
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg';
-import navbar from '../assets/portfolio/navbar.jpg';
-import reactParallax from '../assets/portfolio/reactParallax.jpg';
-
-
+import portfolioImg from '../assets/portfolio/portfolioImg.jpg';
+import fitscorpionImg from '../assets/portfolio/fitscorpionImg.jpg';
+import roybotImg from '../assets/portfolio/roybotImg.jpg';
 
 const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: portfolioImg,
+      demoLink: 'https://mausoomi-roy-12.netlify.app/', // Add the demo link for the first item
+      codeLink: 'https://github.com/Mausoomi12/portfolio-website', // Add the code link for the first item
+    },
+    {
+      id: 2,
+      src: fitscorpionImg,
+      demoLink: 'https://example.com/demo2', // Add the demo link for the second item
+      codeLink: 'https://github.com/Mausoomi12/Fit-scorpion-minor', // Add the code link for the second item
+    },
+    {
+      id: 3,
+      src: roybotImg,
+      demoLink: 'https://roy-bot.vercel.app/', // Add the demo link for the third item
+      codeLink: 'https://github.com/Mausoomi12/Roy-Bot', // Add the code link for the third item
+    },
+  ];
 
-    const portfolios = [
-        {
-            id: 1,
-            src: arrayDestruct,
-        },
-        {
-            id: 2,
-            src: reactParallax,
-        },
-        {
-            id: 3,
-            src: navbar,
-        },
-        
-    ]
-
-
-  return ( 
-  <div
-   name="portfolio" 
-   className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-
-    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+  return (
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
-            <p className="py-6">Check out some of my work right here</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-
-        {
-            portfolios.map (({id, src}) => (
-                
+          {portfolios.map(({ id, src, demoLink, codeLink }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-                <img
-                 src={src} 
-                 alt="" 
-                 className="rounded-md duration-200 hover:scale-105" />
-                <div className="flex items-center justify-center">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
-                </div>
+              <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
+              <div className="flex items-center justify-center">
+                <a href={demoLink} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </a>
+                <a href={codeLink} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </a>
+              </div>
             </div>
-            ))
-        }
-
-           </div>
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>  
             ),
-            href: '/resume.pdf',
+            href: '/mausoomi-resume.pdf',
             style: 'rounded-tr-md',
             download: true,
         },
